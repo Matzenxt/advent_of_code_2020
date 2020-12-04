65650c0734f5f881b6d04c27968ea96ca4ddf958
Matthias Lodner <matthias.lodner@uni-ulm.de>
2020-12-04 11:07:39 +0100

    Day 1 b.

---
+++ src/bin/day_1_b.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    let file = File::open("inputs/day_1.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    for line in reader.lines() {
+        numbers.push(line.unwrap().parse::<i32>().unwrap());
+    }
+
+    compute(numbers);
+}
+
+fn compute(numbers: Vec<i32>) -> i32 {
+    for number1 in &numbers {
+        for number2 in &numbers {
+            for number3 in &numbers {
+                if (number1 + number2 + number3) == 2020 {
+                    println!("{}", number1 * number2 * number3);
+                    return number1 * number2 * number3;
+                }
+            }
+        }
+    }
+
+    0
+}
+
+#[test]
+fn test() {
+    let numbers: Vec<i32> = vec![1721, 979, 366, 299, 675, 1456];
+
+    assert_eq!(compute(numbers), 241861950);
+}
